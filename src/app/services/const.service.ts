a78522b799ac46d058d2eb39f67420e2eecc74a0
Atul sethi <atul@memeflow.net>
2020-03-31 00:32:44 +0530

    Fixed API url

--- src/app/services/const.service.ts
+++ src/app/services/const.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConstService {
-  public baseUrl: string = 'https://tweetsearch-api.herokuapp.com';
+  public baseUrl: string = 'https://tweetobacend.herokuapp.com';
   constructor() {
     if (window.location.hostname == 'localhost') {
       this.baseUrl = 'http://192.168.1.55:8000';
